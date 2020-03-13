a5b376e9d05295799803eab3fd6f5e5105acdf95
Juan <jfuyo@netgo.net>
2020-03-13 13:35:41 -0500

    Añadir y eliminar formas de pago

--- src/main/java/com/mym/consulting/repositories/UsersTeamRepository.java
+++ src/main/java/com/mym/consulting/repositories/UsersTeamRepository.java
@@ -1,7 +1,16 @@
 package com.mym.consulting.repositories;
 
+import com.mym.consulting.entities.EtapasProyecto;
 import com.mym.consulting.entities.UsuariosEquipo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsersTeamRepository extends JpaRepository<UsuariosEquipo, Integer> {
+
+    @Query("SELECT ue FROM UsuariosEquipo ue WHERE ue.id.idEquipo = :teamId ORDER BY ue.id.idEquipo ASC")
+    List<UsuariosEquipo> findByTeam(@Param("teamId") Integer teamId);
+
 }
